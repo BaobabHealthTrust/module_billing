96d391be5184398d71f0724fc104ae7f01fa1f09
Blessings <blessings.mhango@gmail.com>
2013-06-12 14:52:32 +0200

    product, prices and invoice lines relationship

--- app/models/billing_product.rb
+++ app/models/billing_product.rb
@@ -3,8 +3,8 @@ class BillingProduct < ActiveRecord::Base
   set_primary_key :product_id
   include Openmrs
 
-  belongs_to :billing_service, :foreign_key => :service_id
-	belongs_to :drug, :foreign_key => :drug_id
+	belongs_to :billing_category, :foreign_key => :category_id
   belongs_to :billing_product_type, :foreign_key => :product_type_id
-  has_many :billing_prices
+  has_many :billing_prices, :foreign_key => :product_id
+  has_many :billing_invoice_lines
 end
