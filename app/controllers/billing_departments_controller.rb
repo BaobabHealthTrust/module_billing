a9b072b855695191028fa208f22eaea82574aa57
Blessings <blessings.mhango@gmail.com>
2013-06-12 15:56:45 +0200

    deletion of deparments,services and products

--- app/controllers/billing_departments_controller.rb
+++ app/controllers/billing_departments_controller.rb
@@ -77,12 +77,26 @@ class BillingDepartmentsController < ApplicationController
   end
 
   def delete
-      @department = BillingDepartment.find(params[:target_id])
-      @project = get_global_property_value("project.name") rescue "Unknown"
-      @department.void("Voided through #{@project}",Time.now,params[:user_id])
+      department = BillingDepartment.find(params[:target_id])
+      categories = BillingCategory.find_all_by_department_id(department.department_id)
+      project = get_global_property_value("project.name") rescue "Unknown"
+      user_id = params[:user_id]
+      void_time = Time.now
+      void_message = "Voided through #{project}"
+
+      categories.each do |category|
+        category.billing_products.each do |product|
+          product.billing_prices.each do |price|
+            price.void(void_message,void_time,user_id)
+          end
+          product.void(void_message,void_time,user_id)
+        end
+        category.void(void_message,void_time,user_id)
+      end
+      department.void(void_message,void_time,user_id)
+      
       respond_to do |format|
         	format.html { redirect_to "/show_departments?user_id=#{params[:user_id]}" if !params[:user_id].blank? }
-        	format.xml  { render :xml => @department, :status => :created, :location => @department }
       end
   end
 end
