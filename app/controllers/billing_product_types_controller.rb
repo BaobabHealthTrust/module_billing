bcf80f8b04e541f4168f15fb41d778a199576a75
Blessings <blessings.mhango@gmail.com>
2013-06-13 16:41:36 +0200

    added edit,update and delete methods

--- app/controllers/billing_product_types_controller.rb
+++ app/controllers/billing_product_types_controller.rb
@@ -45,18 +45,45 @@ class BillingProductTypesController < ApplicationController
   end
 
   def edit
+    @product_type = BillingProductType.find(params[:target_id])
+    
+		if params[:user_id].nil?
+			redirect_to '/encounters/no_user' and return
+		end
+		@user = User.find(params[:user_id]) rescue nil?
+
+    	respond_to do |format|
+      		format.html # new.html.erb
+      		format.xml  { render :xml => @product_type }
+    	end
   end
 
   def update
-  end
-
-  def edit
-  end
-
-  def update
+      @product_type = BillingProductType.find(params[:product_type_id])
+      @product_type.name = params[:name]
+      
+      respond_to do |format|
+        if @product_type.save
+          format.html { redirect_to "/show_product_types?user_id=#{params[:user_id]}" if !params[:user_id].blank? }
+          format.xml  { render :xml => @product_type, :status => :created, :location => @product_type }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @product_type.errors, :status => :unprocessable_entity }
+        end
+      end
   end
 
   def delete
+      product_type = BillingProductType.find(params[:target_id])
+      project = get_global_property_value("project.name") rescue "Unknown"
+      user_id = params[:user_id]
+      void_time = Time.now
+      void_message = "Voided through #{project}"
+
+      product_type.void(void_message,void_time,user_id)
+      respond_to do |format|
+        	format.html { redirect_to "/show_product_types?user_id=#{params[:user_id]}" if !params[:user_id].blank? }
+      end
   end
 
 end
