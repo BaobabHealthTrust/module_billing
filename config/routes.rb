98d2dd93dc028751594fad069ea5c5df8c3b2647
Blessings <blessings.mhango@gmail.com>
2013-06-13 11:29:50 +0200

    added routes for editing, updating and voiding products

--- config/routes.rb
+++ config/routes.rb
@@ -98,6 +98,12 @@ ActionController::Routing::Routes.draw do |map|
 
   map.new_product '/new_product', :controller => 'billing_product', :action => 'new'
 
+  map.edit_product '/edit_product', :controller => 'billing_product', :action => 'edit'
+
+  map.update_product '/update_product', :controller => 'billing_product', :action => 'update'
+
+  map.void_product '/void_product', :controller => 'billing_product', :action => 'delete'
+
   map.create_product '/create_product', :controller => 'billing_product', :action => 'create'
 
   map.show_product_types '/show_product_types', :controller => 'billing_product_types', :action => 'index'
