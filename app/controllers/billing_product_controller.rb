d2950caf5064cc0f51a395932a4fdd4a3433b6d9
Blessings <blessings.mhango@gmail.com>
2013-06-13 11:23:45 +0200

    billing edit, update and delete

--- app/controllers/billing_product_controller.rb
+++ app/controllers/billing_product_controller.rb
@@ -55,9 +55,57 @@ class BillingProductController < ApplicationController
   end
 
   def edit
+    @product = BillingProduct.find(params[:target_id])
+    @product_type_map = BillingProductType.all.map do |product_type|
+      [product_type.name, product_type.product_type_id] rescue []
+    end
+
+    @category_map = BillingCategory.all.map do |category|
+      [category.billing_department.name + " - " + category.name, category.category_id] rescue []
+    end
+
+		if params[:user_id].nil?
+			redirect_to '/encounters/no_user' and return
+		end
+		@user = User.find(params[:user_id]) rescue nil?
+
+    	respond_to do |format|
+      		format.html # new.html.erb
+      		format.xml  { render :xml => @product }
+    	end
   end
 
   def update
+      @product = BillingProduct.find(params[:product_id])
+      @product.name = params[:name]
+      @product.category_id = params[:category_id]
+      @product.product_type_id = [:product_type]
+      respond_to do |format|
+        if @product.save
+          format.html { redirect_to "/show_products?user_id=#{params[:user_id]}" if !params[:user_id].blank? }
+          format.xml  { render :xml => @product, :status => :created, :location => @product }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @product.errors, :status => :unprocessable_entity }
+        end
+      end
+  end
+
+  def delete
+      product = BillingProduct.find(params[:target_id])
+      prices = BillingPrice.find_all_by_product_id_and_voided(product.product_id, false)
+      project = get_global_property_value("project.name") rescue "Unknown"
+      user_id = params[:user_id]
+      void_time = Time.now
+      void_message = "Voided through #{project}"
+
+      prices.each do |price|
+        price.void(void_message,void_time,user_id)
+      end
+      product.void(void_message,void_time,user_id)
+      respond_to do |format|
+        	format.html { redirect_to "/show_products?user_id=#{params[:user_id]}" if !params[:user_id].blank? }
+      end
   end
 
 end
