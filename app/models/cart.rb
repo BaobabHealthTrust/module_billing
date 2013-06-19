29364abf8b38a205d1a5edd665320ef89cfc2765
Blessings <blessings.mhango@gmail.com>
2013-06-19 05:18:26 +0200

    fixed add product method

---
+++ app/models/cart.rb
@@ -0,0 +1,19 @@
+class Cart
+  attr_reader :items
+
+  def initialize
+      @items = []
+  end
+
+def add_product(product,price_type)
+  current_item = @items.find {|item| item.product == product}
+  if current_item
+    current_item.increment_quantity
+  else
+    @items << CartItem.new(product,price_type)
+  end
+end
+
+end
+
+
