9d02a58ed0338395a5e61b15b14a662af72fe87e
Blessings <blessings.mhango@gmail.com>
2013-06-19 16:58:28 +0200

    billing cart

---
+++ app/models/cart_item.rb
@@ -0,0 +1,26 @@
+
+class CartItem
+  attr_reader :product, :quantity, :price_type
+
+  def initialize(product,price_type)
+    @product = product
+    @quantity = 1
+    @final_price = BillingPrice.find_by_product_id_and_price_type(@product.product_id,price_type)
+  end
+
+  def increment_quantity
+    @quantity += 1
+  end
+
+  def name
+    @product.name
+  end
+  
+  def product_code
+    @product.product_id
+  end
+  
+  def price
+    @final_price.amount * @quantity
+  end
+end
---
+++ test/unit/helpers/billing_cart_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BillingCartHelperTest < ActionView::TestCase
+end
