ca19960f7914710d2af2ad95f469b97f2ebeff7b
Blessings <blessings.mhango@gmail.com>
2013-10-25 13:56:55 +0200

    destroyed admissions model

--- app/models/billing_admissions.rb
+++
@@ -1,8 +0,0 @@
-class BillingAdmissions < ActiveRecord::Base
-	set_table_name :billing_admissions
-  set_primary_key :admissions_id
-  include Openmrs
-
-  belongs_to :invoice_line, :foreign_key => :invoice_line_id
-  belongs_to :billing_price, :foreign_key => :price_id
-end
